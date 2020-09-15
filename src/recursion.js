4dc858dd8c6fafb136e482379e678b6e9aae84ff
Fabian Yee <fabianyee95@gmail.com>
2020-09-15 15:34:57 -0700

    implement sum

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,14 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    var lastNumInd = array.length - 1;
+    var newArray = array.slice(0, lastNumInd);
+
+    if (lastNumInd < 0) {
+        return 0;
+    } else {
+        return array[lastNumInd] + sum(newArray);
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
