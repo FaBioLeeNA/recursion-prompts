e0210add25ec4baf2413b842ee094c0df291bebf
Fabian Yee <fabianyee95@gmail.com>
2020-09-16 17:13:28 -0700

    implement reverse

--- src/recursion.js
+++ src/recursion.js
@@ -116,6 +116,12 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+    if (string.length === 1) {
+        // console.log(string);
+        return string;
+    } else {
+        return string[string.length - 1] + reverse(string.substring(0, string.length - 1));
+    }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
